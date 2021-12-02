5bbcda6e20d34b098740220a453cd66e246b0a1f
Ben Kaan <benkaan001@gmail.com>
2021-12-02 14:24:40 -0600

    set up express connection

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(require("./routes"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/mongoSpace",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+app.listen(PORT, () => {
+  console.log(
+    `🌍🌍🌍🌍🌍🌍🌍🌍 ====> Server Connected on localhost:${PORT} 🌍🌍🌍🌍🌍🌍`
+  );
+});
