d771210d26025e64ebb27d9fe67b6e15927cf065
Ben Kaan <benkaan001@gmail.com>
2021-12-03 00:53:19 -0600

    finish the all the API endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,6 @@
-
+const router = require("express").Router();
+// import all of the API routes from /api/index.js
+const apiRoutes = require("./api");
+// attach prefix of `/api` to all of the api routes imported from the api directory
+router.use("/api", apiRoutes);
+module.exports = router;
