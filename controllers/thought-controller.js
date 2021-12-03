51d5a58bd5e5d6118dba7a4e525e6d4c89aaf0de
Ben Kaan <benkaan001@gmail.com>
2021-12-03 02:08:29 -0600

    set up thought controllers

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,5 +1,50 @@
-const { Thought } = require("../models");
+const { Thought, User } = require("../models");
 
-const thoughtController = {};
+const thoughtController = {
+  // add thought to user
+  addThought({ params, body }, res) {
+    Thought.create(body)
+      .then(({ _id }) => {
+        User.findOneAndUpdate(
+          { _id: params.userId },
+          { $push: { thoughts: _id } },
+          { new: true }
+        );
+      })
+      .then((userData) => {
+        if (!userData) {
+          res.status(404).json({ message: "No user found with this ID!" });
+          return;
+        }
+        res.status(200).json(userData);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // remove thought
+  removeThought({ params }, res) {
+    // findOneAndDelete deletes the document while also returning its data like findOneAndUpdate
+    Thought.findOneAndDelete({ _id: params.thoughtId })
+      .then((deletedThought) => {
+        if (!deletedThought) {
+          res.status(404).json({ message: "No thought found with this ID!" });
+          return;
+        }
+        return User.findOneAndUpdate(
+          { _id: params.userId },
+          { $pull: { thoughts: params.thoughtId } },
+          { new: true }
+        );
+      })
+      .then((userData) => {
+        if (!userData) {
+          res.status(404).json({ message: "No user found with this ID!" });
+          return;
+        }
+        res.status(200).json(userData);
+      })
+      .catch((err) => res.status(500).json(err));
+  },
+};
 
 module.exports = thoughtController;
